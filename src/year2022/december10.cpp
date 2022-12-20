e78c755969e1b31dcbec76acd8827ecd2376df04
Zimon Kuhs <zimonk@axis.com>
2022-12-20 11:28:43 +0100

    Add CRT class to 10.
    Change-Id: Ibf48270e3deb813a4e610c11b1d2560f8ec60c10

--- src/year2022/december10.cpp
+++ src/year2022/december10.cpp
@@ -8,13 +8,13 @@
 #include "files.hpp"
 #include "strings.hpp"
 
-#define str(x) std::to_string(x)
+#define toStr(x) std::to_string(x)
 
 using namespace adventOfCode;
 
 class CPU {
     public:
-        CPU() : signals({}), changes({}), head(nullptr), value(1), cycle(0) {}
+        CPU() : signals({}), changes({}), head(nullptr), data(1), cycle(0) {}
 
         void addX(const int &value) {
             changes.push_back({ 2, value });
@@ -34,21 +34,58 @@ class CPU {
 
         void tick() {
             if (++cycle % 40 == 20) {
-                signals.push_back(value * static_cast<int>(cycle));
+                signals.push_back(data * static_cast<int>(cycle));
             }
 
             std::pair<size_t, int> &change = changes.front();
             if (--change.first == 0) {
-                value += change.second;
+                data += change.second;
                 changes.erase(changes.begin());
             }
+        }
+
+        int value() const {
+            return data;
         }
 
     private:
         std::vector<int> signals;
         std::vector<std::pair<size_t, int>> changes;
         std::unique_ptr<std::pair<size_t, int>> head;
-        int value;
+        int data;
+        size_t cycle;
+};
+
+class CRT {
+    public:
+        CRT() : pixels({{}}), position(0), cycle(0) {}
+
+        std::string string() const {
+            std::stringstream result;
+            for (const std::vector<char> &row : pixels) {
+                for (const char &character : row) {
+                    result << character;
+                }
+                result << std::endl;
+            }
+            return result.str();
+        }
+
+        void tick(const int &modifier) {
+            if (position >= 20) {
+                position = 0;
+                pixels.push_back({});
+            }
+
+            pixels.back().push_back(
+                ++cycle + modifier + 1 <= 2 ? '#' : '.'
+            );
+            position += 1;
+        }
+
+    private:
+        std::vector<std::vector<char>> pixels;
+        size_t position;
         size_t cycle;
 };
 
@@ -73,9 +110,12 @@ std::string Year2022::december10() const {
         }
     }
 
+    CRT crt = CRT();
     while (cpu.busy()) {
         cpu.tick();
+        crt.tick(cpu.value());
     }
+    echo(crt.string());
 
     return std::to_string(vectorSum(cpu.strengths()));
 }
